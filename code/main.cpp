893417d35912ec1d78f175deb173702ac04c7019
Vishal Maurya <vishalkumar.2222@yahoo.in>
2024-09-11 10:01:01 +0200

    Adding Qt files.

--- code/main.cpp
+++ code/main.cpp
@@ -8,6 +8,11 @@
 
 int main(int argc, char** argv){
     QApplication app(argc, argv);
+
+    QWebEngineProfile::defaultProfile()->settings()->setAttribute(QWebEngineSettings::PluginsEnabled, true);
+    QWebEngineProfile::defaultProfile()->settings()->setAttribute(QWebEngineSettings::DnsPrefetchEnabled, true);
+    QWebEngineProfile::defaultProfile()->settings()->setAttribute(QWebEngineSettings::ScreenCaptureEnabled, true);
+
     MainWindow window;
     window.show();
     return app.exec();
